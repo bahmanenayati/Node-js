041dcfc8d21bc032a862962a3ec159fc747a9b31
Bahman <1benayatei@gmail.com>
2019-06-21 13:03:36 +0430

    New dir structure.

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+var app = express()
+
+app.use(function (req, res, next) {
+    console.log('Time:', Date.now())
+    next()
+})
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
+});
+
+module.exports = router;
