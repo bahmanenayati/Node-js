70ddcf03eb2db12c52afe118f1f75ba073010dcc
Bahman <1benayatei@gmail.com>
2019-06-21 23:56:27 +0430

    Create user model and new api for create and get users list from db.

--- routes/user.js
+++ routes/user.js
@@ -3,9 +3,8 @@ var router = express.Router();
 
 const UserController = require('../app/controllers/UserController');
 
-router.get('/', function (req, res, next) {
-    res.send('respond with a resource');
-});
-router.get('/list', UserController.userList);
+router.get('/list', UserController.list);
+router.get('/:id', UserController.findById);
+router.post('/create', UserController.create);
 
 module.exports = router;
