0f477b971931aa03b0862aadb7186da998858bd6
Bahman <1benayatei@gmail.com>
2019-06-28 16:22:57 +0430

    Log http requests into file.

--- app.js
+++ app.js
@@ -3,31 +3,37 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var rfs = require('rotating-file-stream')
 const config = require('config');
-var app = express();
 
 //Database connection
 const mongoose = require('mongoose');
 const dbConfig = config.get('dbConfig');
 mongoose.connect(`${dbConfig.host}:${dbConfig.port}/${dbConfig.dbName}`, {useNewUrlParser: true});
 
-//Routers
 var indexRouter = require('./routes/Index');
 var userRouter = require('./routes/User');
-app.use('/', indexRouter);
-app.use('/user', userRouter);
 
-
+var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
+
+var accessLogStream = rfs('access.log', {
+    interval: '1d', // rotate daily
+    path: path.join(__dirname, 'logs')
+})
+
+// setup the logger
+app.use(logger('combined', {stream: accessLogStream}))
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', indexRouter);
+app.use('/user', userRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
